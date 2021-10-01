2a86a056fe655cd696b7abcba7d471d7493d13d7
Eltun-Ismayilov <eltunhi@code.edu.az>
2021-10-02 00:15:54 +0400

    Test

---
+++ WebApplication1/WebApplication1/Models/Demo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiqnalR.Models
+{
+    public class Demo
+    {
+        public int MyProperty { get; set; }
+    }
+}
